4508c4dff7a426a9b63e1f0e11c9eeb629b3fab4
Aykut Korkmaz <aykutkorkmaz.original@gmail.com>
2020-09-15 00:17:42 +0300

    files added

---
+++ scripts/scripts.js
@@ -0,0 +1,56 @@
+var countDownDate = new Date("Tue Sep 15 2020 20:00:00").getTime();
+
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("mins").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  if (distance < 0) {
+    var countdown_area = document.getElementsByClassName("countdown-container");
+    countdown_area.remove();
+    clearInterval(x);
+    document.getElementById("also_expired").innerHTML = "Special event has started!";
+  }
+}, 1000);
+
+
+/* const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
+const appleEvent = "Tue Sep 15 2020 20:00:00 GMT+0300 (GMT+03:00)";
+
+function countdown() {
+    const appleEventDate = new Date(appleEvent);
+    const currentDate = new Date();
+    console.log(currentDate);
+
+    const totalSeconds = (appleEventDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+countdown();
+setInterval(countdown, 1000); */
